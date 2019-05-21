9ea54f1747f79360db1eeff563f8e2d5adf50db0
JAYRAJ ARORA <jayraja@mindfiresolutions.com>
2019-05-21 18:34:59 +0530

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  // query by key value pairs and toArray converts the cursor to array of documents
+  // db.collection('Todos').find(
+  //   {_id: new ObjectID('5ce3f1f4346042a9a9e5daf9')}
+  // ).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'jka'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch data', err);
+  })
+  console.log('Connected to mongodb server');
+
+  // db.close();
+});
