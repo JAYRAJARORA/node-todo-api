e9293ab60fb7bc9f4609fc2c1f4f4e8d7220d638
Jayraj Arora <jayraj.arora@gmail.com>
2019-05-31 21:44:24 -0700

    test ssh keys are set

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
+  console.log('here');
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
   process.env.PORT = 3000;
